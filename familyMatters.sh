be7d1012008022d4e40b709954d7b5397fa63e8f
Chinyere Ukomadu <CUkomadu@Chinyeres-MacBook-Pro.local>
2016-09-27 14:44:32 -0500

    add familyMatters shell script file

---
+++ familyMatters.sh
@@ -0,0 +1,14 @@
+mkdir grandpa
+mkdir grandpa/pa
+mkdir grandpa/ma
+mkdir grandpa/ma/baby
+touch grandpa/README.txt
+curl http://i.giphy.com/l2YWBV91Tukbswc5W.gif > nightmare.gif
+curl http://i.giphy.com/l0MYFFxatWH2ztsQM.gif > hillary.gif
+curl http://i.giphy.com/xTiTnHXbRoaZ1B1Mo8.gif > trumpjoker.gif
+echo justin richards, tarpon springs, florida > justin.txt
+echo chichi ukomadu, katy, tx > chichi.txt
+echo emre cullu, istanbul, turkey > emre.txt
+mv *.gif grandpa/pa
+mv *.txt grandpa/ma
+touch grandpa/ma/baby/gaga.googoo.txt
